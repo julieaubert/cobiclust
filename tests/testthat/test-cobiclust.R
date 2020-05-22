cfcd6ea797c2c80de404928ed3e23152ed80db47
juaubert <julie.aubert@agroparistech.fr>
2020-05-22 16:20:40 +0200

    Update unit tests

--- tests/testthat/test-cobiclust.R
+++ tests/testthat/test-cobiclust.R
@@ -1,3 +1,9 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("arguments for cobiclust main function", {
+
+  ## parameters
+  expect_is(x, "matrix")
+  expect_true(K >= 1)
+  expect_true(K < nrow(x))
+  expect_true(G >= 1)
+  expect_true(K < ncol(x))
 })
